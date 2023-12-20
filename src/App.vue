a9e68388285ae65d1bc5d0fa2b29c0c27f165de5
SeongBeom <vnvnwn@openmate.co.kr>
2023-12-20 14:37:30 +0900

    블로그 만들기, props 복습

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,67 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">블로그</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">홈</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">기록</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              추가메뉴
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">일일 기록</a></li>
+              <li><a class="dropdown-item" href="#">정보</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li><a class="dropdown-item" href="#">이 외 게시글</a></li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+          </li>
+        </ul>
+        <form class="d-flex" role="search">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+
+  <div class="container mt-4">
+    <h5>블로그입니다.</h5>
+    <p>- Vue로 만듦 -</p> 
+  </div>
+
+  <List :블로그글="블로그글" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import List from './components/List.vue';
+import blog from './assets/blog.js';
 
 export default {
   name: 'App',
+  data(){
+    return{
+      블로그글 : blog
+    }
+  },
   components: {
-    HelloWorld
-  }
+    List : List,
+  },
+  
 }
 </script>
+
 
 <style>
 #app {
@@ -21,6 +70,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
